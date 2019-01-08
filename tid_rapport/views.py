e120041ea9b1297e1cba134cfa9ac06f1b808f06
freced81 <freced81@gmail.com>
2019-01-08 21:13:39 +0100

    en till

--- tid_rapport/views.py
+++ tid_rapport/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Tid
 
-# Create your views here.
+
+class TidList(ListView):
+    model = Tid
+    context_object_name = 'tid_rapport'
+    template_name = 'tid.html'
